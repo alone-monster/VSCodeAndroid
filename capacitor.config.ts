0b76a3e0a6260a2c246e501f80f553e79f79b070
alone-monster <chiragnaskarstyle2010@gmail.com>
2026-04-29 14:12:19 +0530

    Add android platform and capacitor config

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.alonemonster.vscode',
+  appName: 'VSCode Mobile',
+  webDir: 'build'
+};
+
+export default config;
